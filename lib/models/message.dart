4f3522334f654f817b92ea1d5e80bc4aa855c2b5
hima hisham <me2722067@gmail.com>
2023-04-28 21:44:22 +0300

    initial commit

---
+++ lib/models/message.dart
@@ -0,0 +1,13 @@
+
+
+import '../contants.dart';
+
+class Message {
+  final String message;
+  final String id;
+  Message(this.message, this.id);
+
+  factory Message.fromJson(jsonData) {
+    return Message(jsonData[kMessage], jsonData['id']);
+  }
+}
